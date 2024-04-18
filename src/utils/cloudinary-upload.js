0f12bdedeb2236fdd16e9a910dc3382373a623da
Vladislav Marinov <104770295+This-Marinov-guy@users.noreply.github.com>
2024-04-18 14:06:35 +0300

    Update cloudinary-upload.js

--- src/utils/cloudinary-upload.js
+++ src/utils/cloudinary-upload.js
@@ -2,31 +2,20 @@ import cloudinary from './cloudinary.js';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 
-// Function to upload images and return their URLs
-export async function uploadImages(files, folder, format ='jpg', id = '10010') {
-
-  const parser = multer({ storage: storage });
-
-
-  // Configure Cloudinary Storage
+export async function imageParser(folder, format ='jpg', id = '10010') {
 const storage = new CloudinaryStorage({
-cloudinary: cloudinary,
-params: {
-folder: folder,
-format: async (req, file) => format, // or other formats
-public_id: (req, file) => id, // generate a unique ID for each file
-},
+	cloudinary: cloudinary,
+	params: {
+		folder: folder,
+		format: async (req, file) => format, // supports promises as well
+		public_id: (req, file) => id,
+	},
 });
-const uploader = async (path) => await cloudinary.uploader.upload(path);
 
-// Map over the files and upload each one
-const uploadPromises = files.map(file => uploader(file.path));
-
-// Wait for all uploads to finish
-const imageResponses = await Promise.all(uploadPromises);
-
-// Extract the secure URLs from the responses
-const imageUrls = imageResponses.map(res => res.secure_url);
-
-return imageUrls;
+return multer({ storage: storage });
 }
+
+// used like
+// const parser = imageParser(...);
+// .parser.single('image') 
+// then access with req.files or req.files
