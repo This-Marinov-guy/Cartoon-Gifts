e6c9810db6ea593399d6e3b094fbb89824518d4c
Vladislav Marinov <104770295+This-Marinov-guy@users.noreply.github.com>
2024-04-18 11:26:42 +0300

    Update cloudinary-upload.js

--- src/utils/cloudinary-upload.js
+++ src/utils/cloudinary-upload.js
@@ -1,18 +1,39 @@
-import cloudinary from 'cloudinary';
+import nextConnect from 'next-connect';
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-cloudinary.config({
+export const uploadMultipleImages = async (files, folder, format = 'jpg', public_id = '010') => {
+    cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
 });
 
-export const uploadImage = async (imagePath, options = {}) => {
+// Configure Cloudinary Storage
+const storage = new CloudinaryStorage({
+cloudinary: cloudinary,
+params: {
+folder: folder,
+format: async (req, file) => format, 
+public_id: (req, file) => public_id,
+},
+});
+
+const parser = multer({ storage: storage });
+    
     try {
-        const result = await cloudinary.uploader.upload(imagePath, options);
-        console.log(result);
-        return result.public_id;
-    } catch (error) {
-        console.error(error);
-    }
+const files = await new Promise((resolve, reject) => {
+parser.array('images')(req, {}, err => {
+if (err) {
+reject(err);
+}
+resolve(req.files);
+});
+});
+res.json(files);
+} catch (error) {
+res.status(500).json({ error: error.message });
+}
 };
