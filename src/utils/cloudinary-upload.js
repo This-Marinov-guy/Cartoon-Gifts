b19e8ffed146b3295ac9c6e712f94f052cac6a4a
Vladislav Marinov <104770295+This-Marinov-guy@users.noreply.github.com>
2024-04-18 13:43:47 +0300

    Update cloudinary-upload.js

--- src/utils/cloudinary-upload.js
+++ src/utils/cloudinary-upload.js
@@ -1,13 +1,6 @@
-import { v2 as cloudinary } from 'cloudinary';
+import cloudinary from './cloudinary.js';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
-
-// Configure Cloudinary
-cloudinary.config({
-cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-api_key: process.env.CLOUDINARY_API_KEY,
-api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 // Function to upload images and return their URLs
 export async function uploadImages(files, folder, format ='jpg', id = '10010') {
