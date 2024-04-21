792fd2f0c078d7587d3d814c98748822387dddef
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-04-21 15:05:55 +0300

    modified:   src/ui/google-translate.jsx

--- src/ui/google-translate.jsx
+++ src/ui/google-translate.jsx
@@ -4,6 +4,25 @@ import icon_globe from "@assets/images/icons/icon_globe.svg";
 import Image from "next/image";
 
 const GoogleTranslate = (props) => {
+    const googleTranslateElementInit = () => {
+        new window.google.translate.TranslateElement(
+            {
+                pageLanguage: "en",
+                includedLanguages: "en,bg,ro",
+            },
+            "google_translate_element"
+        );
+    };
+
+    useEffect(() => {
+        var addScript = document.createElement("script");
+        addScript.setAttribute(
+            "src",
+            "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        );
+        document.body.appendChild(addScript);
+        window.googleTranslateElementInit = googleTranslateElementInit;
+    }, []);
 
     return (
         <div className="center_div">
