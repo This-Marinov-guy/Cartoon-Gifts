c0958fdbdd0abaab54254ef221e4994af67055c6
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-05-18 22:49:15 +0300

    modified:   src/pages/_app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -18,40 +18,11 @@ function MyApp({ Component, pageProps }) {
           "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
         }
       />
-      {/* <Script
-        src="https://cdn.weglot.com/weglot.min.js"
-        strategy="lazyOnload" // Other options: "beforeInteractive", "afterInteractive", "lazyOnload"
-        onLoad={() => Weglot.initialize({
-          api_key: 'wg_de22bcb9e9ad87316072cfbeec7c3f062',
-          hide_switcher: true,
-          // switchers: [
-          //   {
-          //     style: {
-          //       full_name: false,
-          //       with_name: true,
-          //       is_dropdown: true,
-          //       with_flags: true,
-          //       flag_type: "circle",
-          //       invert_flags: false
-          //     },
-          //     // Move switcher somewhere in the page
-          //     location: {
-          //       target: ".translate-switcher",
-          //       sibling: null
-          //     }
-          //   }
-          // ],
-          cache: true,
-          auto_switch: true,
-          auto_switch_fallback: "en",
-          wait_transition: true,
-        })}
-      /> */}
       <Script src="//cdn.conveythis.com/javascript/conveythis-initializer.js"
         strategy="lazyOnload" // Other options: "beforeInteractive", "afterInteractive", "lazyOnload"
         onLoad={() =>
           ConveyThis_Initializer.init({
-            api_key: process.env.NEXT_PUBLIC_CONVEY_THIS
+            api_key: 'pub_af1f4825a77a06596ebd36af4110510f'
           })
         }
       />
