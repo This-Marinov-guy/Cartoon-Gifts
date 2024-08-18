cbbc5ec6c0b709ba19884eed22a3054a6707081c
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-08-18 07:42:01 +0300

    update promo code

--- src/components/common/inputs/PromoCode.jsx
+++ src/components/common/inputs/PromoCode.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { Badge } from '@chakra-ui/react'
+import { Spinner } from '@chakra-ui/react';
 import useTranslation from 'next-translate/useTranslation';
 import { useHttpClient } from '@hooks/use-http-request';
 import { useStore } from 'src/stores/storeContext';
@@ -7,6 +8,7 @@ import { useToast } from '@chakra-ui/react';
 
 const PromoCode = () => {
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
     const { t } = useTranslation('components');
 
     const { checkoutStore } = useStore();
@@ -20,6 +22,7 @@ const PromoCode = () => {
             return;
         }
 
+        setError(false);
         setLoading(true);
         try {
             const responseData = await sendRequest('/api/common/validate-promo-code', "POST" ,{ promoCode: e.target.value });
@@ -32,6 +35,7 @@ const PromoCode = () => {
                     isClosable: true,
                 })
             } else {
+                setError(true);
                 toast({
                     title: t('extra-page.order.promo.invalidCode'),
                     status: 'error',
@@ -52,14 +56,14 @@ const PromoCode = () => {
 
     return (
         <div className="col col-md-6">
-            <h4>{t('extra-page.order.promo.promoCode')} <i className="fa-solid fa-percent"></i></h4>
+            <h4>{t('extra-page.order.promo.promoCode')} {loading ? <Spinner /> : <i className="fa-solid fa-percent"></i>}</h4>
             <div className="form-group m-0">
                 <input
                     type='text'
                     name='promoCode'
                     onBlur={handleInput}
                     placeholder={t('extra-page.order.promo.enterPromoCode')}
-                    className={`form-control ${(false) && 'error-border'}`}
+                    className={`form-control ${error && 'error-border'}`}
                 />
             </div>
             {checkoutStore.checkout.promoCode.value && <Badge onClick={removePromoCode} colorScheme='purple' style={{cursor: 'pointer'}}>{checkoutStore.checkout.promoCode.value} x</Badge>}
